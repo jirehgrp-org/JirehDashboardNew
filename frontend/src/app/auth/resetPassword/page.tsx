e99210c366b2e05b89a9738842a29abb1f3ec441
Kenean Alemayhu <keneanalemayhu1@gmail.com>
2025-01-19 03:37:20 +0300

    updated auth with forgot and reset password

--- frontend/src/app/auth/resetPassword/page.tsx
+++ frontend/src/app/auth/resetPassword/page.tsx
@@ -1,9 +1,9 @@
-import { LoginForm } from "@/components/auth/ResetPasswordForm";
+import { ResetPasswordForm } from "@/components/auth/ResetPasswordForm";
 
 export default function Page() {
   return (
     <div className="flex h-screen w-full items-center justify-center px-4">
-      <LoginForm />
+      <ResetPasswordForm />
     </div>
   );
 }

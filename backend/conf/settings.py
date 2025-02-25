29d8efd19dab4258cff237fab0b04b72ae9436b0
Kenean Alemayhu <keneanalemayhu1@gmail.com>
2025-02-25 18:16:28 +0300

    updated settings.py to allow 157.90.126.193 as a host

--- backend/conf/settings.py
+++ backend/conf/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = env("DJANGO_SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env.bool('DJANGO_DEBUG')
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', '157.90.126.193']
 
 
 # Application definition

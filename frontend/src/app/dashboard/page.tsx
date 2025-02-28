dcafdd4c3eb393cd12bb9a5c0269f834ed145b6a
Kenean Alemayhu <keneanalemayhu1@gmail.com>
2025-02-28 16:20:47 +0300

    production update

--- frontend/src/app/dashboard/page.tsx
+++ frontend/src/app/dashboard/page.tsx
@@ -2,17 +2,15 @@
 
 "use client";
 import { useEffect } from "react";
-import { useRouter } from "next/navigation"; // Import useRouter from next/navigation
+import { useRouter } from "next/navigation";
 
 const Dashboard = () => {
   const router = useRouter(); // Initialize the router
 
-  // Redirect to /dashboard/overview on component mount
   useEffect(() => {
     router.push("/dashboard/overview");
   }, [router]);
 
-  // Render nothing (or a loading state if desired)
   return null;
 };
 
